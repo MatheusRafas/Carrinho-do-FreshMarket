b8d44663f4aa41623baa2af8535ef1117a5a3e7b
Matheus Rafael <matheus_rafas@outlook.com>
2024-07-03 21:04:22 -0300

    Update provider.js

--- src/provider.js
+++ src/provider.js
@@ -1,6 +1,6 @@
 import axios from "axios"; 
 
 export const api = axios.create({
-    baseURL: 'https://crudcrud.com/api/30d9101a29564bcea41beccffa90968a/',
+    baseURL: 'https://crudcrud.com/api/8eb8f82d895c4ad1ad44d367c8cc9986/',
     timeout: 10000,
 });
